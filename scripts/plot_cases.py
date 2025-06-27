67d71a9a60f083871b6ad9461d52b8ae2e04049a
Guilherme Cutieri <cutieriguilherme@gmail.com>
2025-06-27 00:10:35 -0300

    making graphics

--- scripts/plot_cases.py
+++ scripts/plot_cases.py
@@ -15,4 +15,41 @@ class PlotPerDate:
         plt.xlabel('Data')
         plt.ylabel('Novos Casos')
         plt.grid(True)
+        plt.show()
+
+class PlotTotalPerState:
+    def __init__(self,df):
+        self.df = df
+
+    def plotTotal(self):
+        total_per_state = self.df.groupby('state')['newCases'].sum().sort_values(ascending=False)
+        total_per_state.plot(
+            kind = 'bar',
+            figsize=(10, 5),
+            title=f'Total cases per state',
+            color = 'red'
+        )
+        plt.xlabel('State')
+        plt.ylabel('Cases')
+        plt.grid(axis= 'y')
         plt.show()  
+
+class DeathsPerDay:
+    def __init__(self, df, state):
+        self.df = df
+        self.state = state
+
+    def deaths(self):
+        state_df = self.df[self.df['state'] == self.state]
+
+        state_df['date'] = pd.to_datetime(state_df['date'])
+        state_df = state_df.sort_values('date')
+
+        plt.figure(figsize=(12, 5))
+        plt.plot(state_df['date'], state_df['newDeaths'], color='crimson')
+        plt.title(f'Mortes diárias por COVID-19 - {self.state}')
+        plt.xlabel('Data')
+        plt.ylabel('Novas Mortes')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
